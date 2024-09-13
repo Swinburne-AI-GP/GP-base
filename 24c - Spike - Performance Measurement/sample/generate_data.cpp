74942fbc2273a38e206e8b3652e14d81ee4194de
James Bonner <jabcmes@outlook.com>
2024-09-14 02:09:38 +1000

    Sample code for Spike 24

---
+++ 24c - Spike - Performance Measurement/sample/generate_data.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <string>
+#include <vector>
+#include <map>
+#include "includes/json.hpp"
+
+// For convenience
+using json = nlohmann::json;
+using namespace std;
+
+random_device rd;
+mt19937 generator(rd());
+
+vector<string> olympian_names = {
+    "Zeus",
+    "Hera",
+    "Poseidon",
+    "Demeter",
+    "Athena",
+    "Apollo",
+    "Artemis",
+    "Ares",
+    "Aphrodite",
+    "Hephaestus",
+    "Hermes",
+    "Hestia",
+    "Dionysus",
+    "Hades"
+};
+
+// Function to generate a random string of given length
+string generateRandomString(size_t length) {
+    const std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    std::uniform_int_distribution<> distribution(0, characters.size() - 1);
+
+    std::string randomString;
+    for (size_t i = 0; i < length; ++i) {
+        randomString += characters[distribution(generator)];
+    }
+    return randomString;
+}
+
+// Function to generate a random social_post JSON object
+json generateRandomsocial_post() {
+    std::uniform_int_distribution<> dis(1, 200);
+
+    json social_post;
+    social_post["id"] = generateRandomString(10);
+    int random_number = dis(generator);
+	if (random_number < 14) {
+        social_post["user"]["name"] = olympian_names[random_number - 1];
+	}
+	else {
+        social_post["user"]["name"] = generateRandomString(10);
+	}
+    social_post["text"] = generateRandomString(200);
+    social_post["created_at"] = "Wed Oct 10 20:19:24 +0000 2024";
+    return social_post;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <number_of_social_posts>" << std::endl;
+        return 1;
+    }
+
+    int numberOfsocial_posts = std::stoi(argv[1]);
+    std::vector<json> social_posts;
+
+    for (int i = 0; i < numberOfsocial_posts; ++i) {
+        social_posts.push_back(generateRandomsocial_post());
+    }
+
+    json output = social_posts;
+    std::ofstream file("./data/social_posts.json");
+    file << output.dump(4); // Pretty print with 4 spaces
+    file.close();
+
+    std::cout << "Generated " << numberOfsocial_posts << " social_posts and saved to social_posts.json" << std::endl;
+    return 0;
+}
---
+++ 24c - Spike - Performance Measurement/sample/performance_test.cpp
@@ -0,0 +1,152 @@
+// performance_test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <vector>
+#include "includes/json.hpp"
+
+// For convenience
+using json = nlohmann::json;
+using namespace std;
+
+vector<string> olympian_names = {
+	"Zeus",
+	"Hera",
+	"Poseidon",
+	"Demeter",
+	"Athena",
+	"Apollo",
+	"Artemis",
+	"Ares",
+	"Aphrodite",
+	"Hephaestus",
+	"Hermes",
+	"Hestia",
+	"Dionysus",
+	"Hades"
+};
+
+// Function to read JSON from a file
+json readJsonFromFile(const std::string& filename) {
+	std::ifstream file(filename);
+	json j;
+	file >> j;
+	return j;
+}
+
+// Method 1: Search using a simple loop
+int searchJsonSimpleLoop(const json& jsonData) {
+	int olympians_found = 0;
+	for (const auto& olympian : olympian_names) {
+		for (const auto& item : jsonData) {
+			if (item.is_object() && item.contains("user")) {
+				const auto& user = item["user"];
+				if (user.is_object() && user.contains("name")) {
+					const auto& name = user["name"];
+					if (name.is_string() && name.get<std::string>() == olympian) {
+						olympians_found++;
+					}
+				}
+			}
+		}
+	}
+	return olympians_found;
+}
+
+// Method 2: Inside-out simple loop
+int searchJsonInsideOutSimpleLoop(const json& jsonData) {
+	int olympians_found = 0;
+	for (const auto& item : jsonData) {
+		if (item.is_object() && item.contains("user")) {
+			const auto& user = item["user"];
+			if (user.is_object() && user.contains("name")) {
+				const auto& name = user["name"];
+				for (const auto& olympian : olympian_names) {
+					if (name.is_string() && name.get<std::string>() == olympian) {
+						olympians_found++;
+					}
+				}
+			}
+		}
+	}
+	return olympians_found;
+}
+
+// Method 3: Search using a recursive function
+int searchJsonRecursive(const json& jsonData, int& olympians_found) {
+	if (jsonData.is_string()) {
+		auto it = std::find(olympian_names.begin(), olympian_names.end(), jsonData.get<std::string>());
+		if (it != olympian_names.end()) {
+			return olympians_found++;
+		}
+	}
+	for (const auto& item : jsonData) {
+		return searchJsonRecursive(item, olympians_found);
+	}
+}
+int searchJsonRecursive(const json& jsonData) {
+	int olympians_found = 0;
+	return searchJsonRecursive(jsonData, olympians_found);
+}
+
+// Method 4: Search using string search
+int searchString(const json& jsonData) {
+	int olympians_found = 0;
+	std::ifstream file("./data/social_posts.json");
+	std::string line;
+	while (std::getline(file, line)) {
+		for (const auto& olympian : olympian_names) {
+			if (line.find(olympian) != std::string::npos) {
+				olympians_found++;
+			}
+		}
+	}
+	return olympians_found;
+}
+
+void measure_performance(
+	//function to be tested
+	string function_name,
+	int (*test_function)(const json&),
+	//data to pass into test function
+	const json& j
+) {
+	auto start = chrono::high_resolution_clock::now();
+	int olympians_found = test_function(j);
+	cout << olympians_found << " olympians found" << endl;
+	auto end = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+	cout << function_name << ": " << duration << " microseconds" << endl;
+}
+
+int main() {
+	// Read JSON data from file
+	json j = readJsonFromFile("./data/social_posts.json");
+
+	// Measure performance of simple loop
+	measure_performance("Simple Loop", searchJsonSimpleLoop, j);
+	measure_performance("Simple Loop", searchJsonSimpleLoop, j);
+	measure_performance("Simple Loop", searchJsonSimpleLoop, j);
+	measure_performance("Simple Loop", searchJsonSimpleLoop, j);
+
+	// Measure performance of inside-out simple loop
+	measure_performance("Inside-Out Simple Loop", searchJsonInsideOutSimpleLoop, j);
+	measure_performance("Inside-Out Simple Loop", searchJsonInsideOutSimpleLoop, j);
+	measure_performance("Inside-Out Simple Loop", searchJsonInsideOutSimpleLoop, j);
+	measure_performance("Inside-Out Simple Loop", searchJsonInsideOutSimpleLoop, j);
+
+
+	// Measure performance of string search
+	measure_performance("String Search", searchString, j);
+	measure_performance("String Search", searchString, j);
+	measure_performance("String Search", searchString, j);
+	measure_performance("String Search", searchString, j);
+
+	// Measure performance of recursive function
+	//measure_performance("Recursive Function", searchJsonRecursive, j);
+
+	return 0;
+}
